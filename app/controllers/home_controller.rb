a71bb51e2c3004200901a00cb8973dde064ca3e7
ANGEL <acostalarangeira@gmail.com>
2019-06-16 22:31:13 -0300

    add navbar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,3 @@
 class HomeController < GeofencingsController
-  def index
-  end
+  def index; end
 end
--- spec/features/visit_home_spec.rb
+++ spec/features/visit_home_spec.rb
@@ -2,10 +2,7 @@ require 'rails_helper'
 
 feature 'visit home' do
   scenario 'successfully' do
-    #dados
-    #navega
     visit root_path
-    #expectativa
     expect(page).to have_content('Welcome to the Timetracking Challenge')
   end
 end
